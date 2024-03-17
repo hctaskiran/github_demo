7650960baebf1d4add1f761934c3e98c2d92ea39
Haktan Can Taşkıran <haktancantaskiran@gmail.com>
2024-03-17 13:17:06 +0300

    Initial Commit

---
+++ github_demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  github_demo
+//
+//  Created by Haktan Can Taşkıran on 17.03.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ github_demo/github_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  github_demoApp.swift
+//  github_demo
+//
+//  Created by Haktan Can Taşkıran on 17.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct github_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
